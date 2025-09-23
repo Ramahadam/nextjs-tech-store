1f20bbb7ba03fbed866ae7ea9f2112707736b54e
Mohamed <ramy.adam33@gmail.com>
2025-09-23 06:12:53 +0400

    feat:update gallary component to dynamicly display the selected product image

--- components/Gallary.tsx
+++ components/Gallary.tsx
@@ -1,17 +1,20 @@
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 interface ImagesType {
   images: Array<string>;
 }
 
 export default function Gallary({ images }: ImagesType) {
+  const [displayedImage, setDisplayedImage] = useState(images?.at(0));
   return (
     <div className="gallary flex flex-col gap-18 items-center ">
       <figure className="bg-white p-8">
         <Image
           priority
-          src={images?.at(0)}
+          src={`${displayedImage}`}
           width={0}
           height={0}
           alt="image for laptop & desktop"
@@ -24,8 +27,9 @@ export default function Gallary({ images }: ImagesType) {
           <Button
             variant="ghost"
             key={idx}
+            onClick={() => setDisplayedImage(el)}
             className={`bg-white h-fit max-w-fit px-0 ${
-              idx == 0 && "border border-primary"
+              el == displayedImage && "border border-primary"
             } `}
           >
             <Image
