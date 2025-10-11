4eb23a8f05ff5e0905b4b78fc4c6ab222963e959
Mohamed <ramy.adam33@gmail.com>
2025-10-11 17:10:48 +0400

    feat: integrate Firebase authentication and configuration

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import FilterSortOperations from "@/components/FilterSortOperations";
 import HeroSection from "@/components/HeroSection";
 import PopularItemsSection from "@/components/PopularItemsSection";
 import ProductList from "@/components/product/ProductList";
-
 export default function Home() {
   return (
     <>
