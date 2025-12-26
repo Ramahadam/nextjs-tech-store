a0652e90e4606574dc33dc25a358b6cbdf758f73
Mohamed <ramy.adam33@gmail.com>
2025-12-26 20:51:30 +0400

    refactor(proxy): remove the wishlist from matcher since it's nested route to /me

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/cart/:path*", "/me/:path*", "/wishlist"],
+  matcher: ["/cart/:path*", "/me/:path*"],
 };
 
 export function proxy(request: NextRequest) {
