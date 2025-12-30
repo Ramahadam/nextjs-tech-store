78d78233106cc9367ac5597bd7b610fb44145c68
Mohamed <ramy.adam33@gmail.com>
2025-12-30 15:04:16 +0400

    feat(auth): hyderate redux from firebase auth state

--- lib/firebase/useOnAuthStateChanged.ts
+++ lib/firebase/useOnAuthStateChanged.ts
@@ -2,22 +2,34 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { auth } from "./config";
+import { useAppDispatch } from "@/app/hooks";
+import { logout, setProfile } from "@/features/user/userSlice";
+import { setCredntials } from "@/features/auth/authSlice";
+import { useSyncUserMutation } from "@/features/api/apiSlice";
 
 export function useOnAuthStateChanged() {
+  const [syncUser] = useSyncUserMutation();
+  const disptach = useAppDispatch();
+
   useEffect(() => {
-    const unbscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        console.log("Logged in successfuly");
-        // const token = await user.getIdToken();
-
-        // store.dispatch(setCredntials(token));
-
-        // const profile = (await syncUser(token)).data?.user[0];
-
-        // store.dispatch(setProfile(profile));
-      } else {
-        console.log("There is no logged in user ");
+    const unbscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (!firebaseUser) {
+        disptach(logout());
+        return;
       }
+
+      const token = await firebaseUser.getIdToken();
+
+      if (!token) return;
+
+      disptach(setCredntials(token));
+
+      // 3. Sync user with backend
+      const profile = await syncUser({
+        token,
+      }).unwrap();
+
+      if (profile) disptach(setProfile(profile.user));
     });
 
     return () => unbscribe();
