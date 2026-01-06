21d33854e28af08d65c1d9eaac4afa8f666751d4
Mohamed <ramy.adam33@gmail.com>
2026-01-06 11:30:33 +0400

    refactor(cart): split handleupdate into decrease and increase function

--- components/QuantityButton.tsx
+++ components/QuantityButton.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useUpdateCartQuantityMutation } from "@/features/api/apiSlice";
-// import {
-//   decreaseItemQuantity,
-//   increaseItemQuantity,
-// } from "@/features/cart/cartSlice";
+
 import { Minus, Plus } from "lucide-react";
 
 type QuantityButtonType = {
@@ -12,19 +9,29 @@ type QuantityButtonType = {
   quantity: number;
 };
 
-export default function QuantityButton({ id, quantity }: QuantityButtonType) {
-  const [updateCartQuantity] = useUpdateCartQuantityMutation();
+const MIN_QTY = 1;
 
-  async function handleUpdateCart(productId: string, quantity: number) {
-    await updateCartQuantity({ productId, quantity });
-  }
+export default function QuantityButton({ id, quantity }: QuantityButtonType) {
+  const [updateCartQuantity, { isLoading, isError, error, isSuccess }] =
+    useUpdateCartQuantityMutation();
+
+  const handleDecrease = async (productId: string, quantity: number) => {
+    if (quantity <= MIN_QTY) return;
+    await updateCartQuantity({ productId, quantity: quantity - 1 });
+  };
+  const handleIncrease = async (productId: string, quantity: number) => {
+    await updateCartQuantity({ productId, quantity: quantity + 1 });
+  };
+
+  // TODO:Later will use sooner to display success and error messages
 
   return (
     <div className="flex items-center border rounded-md justify-between w-fit h-8">
       <Button
         size="icon"
         variant="ghost"
-        onClick={() => handleUpdateCart(id, quantity - 1)}
+        onClick={() => handleDecrease(id, quantity)}
+        disabled={isLoading || quantity <= MIN_QTY}
       >
         <Minus className="size-3" />
       </Button>
@@ -32,7 +39,8 @@ export default function QuantityButton({ id, quantity }: QuantityButtonType) {
       <Button
         size="icon"
         variant="ghost"
-        onClick={() => handleUpdateCart(id, quantity + 1)}
+        onClick={() => handleIncrease(id, quantity)}
+        disabled={isLoading}
       >
         <Plus className="size-3" />
       </Button>
