2fe0129ad3f697f5ada9a01f0636dd5a8a82e226
Mohamed <ramy.adam33@gmail.com>
2026-01-21 16:45:28 +0400

    fix(test): update assertion to expect the button  on loading

--- features/auth/LoginForm.test.tsx
+++ features/auth/LoginForm.test.tsx
@@ -48,9 +48,10 @@ describe("LoginForm", () => {
       expect(screen.getByLabelText("loading")).toBeInTheDocument();
       expect(
         screen.queryByRole("button", { name: /login/i }),
-      ).not.toBeInTheDocument();
+      ).toBeInTheDocument();
     });
   });
+
   describe("when firebase error occur", () => {
     beforeEach(() => {
       (useAuthActions as jest.Mock).mockReturnValue({
@@ -66,6 +67,57 @@ describe("LoginForm", () => {
       expect(
         screen.getByText(/Invalid email or password/i),
       ).toBeInTheDocument();
+    });
+  });
+
+  describe("when empty submit", () => {
+    beforeEach(() => {
+      (useAuthActions as jest.Mock).mockReturnValue({
+        login: jest.fn(),
+        isLoading: true,
+        authError: "email is required",
+        setAuthError: jest.fn(),
+      });
+    });
+    it("email is required", () => {
+      render(<LoginForm />);
+
+      expect(screen.getByText(/email is required/i)).toBeInTheDocument();
+    });
+  });
+
+  describe("when fill and submit", () => {
+    const handleLoginMock = jest.fn();
+
+    (useAuthActions as jest.Mock).mockReturnValue({
+      login: jest.fn(),
+      handleLoginMock: handleLoginMock,
+      isLoading: false,
+      authError: null,
+      setAuthError: jest.fn(),
+    });
+
+    it("submits form with correct values", () => {
+      // Arrange
+      render(<LoginForm />);
+
+      //Act
+      fireEvent.change(screen.getByLabelText(/email/i), {
+        target: { value: "test@gmail.com" },
+      });
+
+      fireEvent.change(screen.getByLabelText(/password/i), {
+        target: "12345678",
+      });
+
+      fireEvent.click(screen.getByRole("button", { name: /login/i }));
+
+      // Assert
+      expect(handleLoginMock).toHaveBeenCalledTimes(1);
+      expect(handleLoginMock).toHaveBeenCalledWith({
+        email: "test@gmail.com",
+        passwrod: "12345678",
+      });
     });
   });
 });
