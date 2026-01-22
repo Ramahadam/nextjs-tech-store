53ddcd0f926bb9dd3f2eff6d369e7dd97862c0bc
Mohamed <ramy.adam33@gmail.com>
2026-01-22 20:56:42 +0400

    test(auth): add unit test verifying form submition behavior

--- features/auth/SignupForm.test.tsx
+++ features/auth/SignupForm.test.tsx
@@ -1,7 +1,7 @@
 import { SignupForm } from "./SignupForm";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { useAuthActions } from "@/features/auth/hooks/useAuthActions";
-
 // jest.mock("next/navigation", () => ({
 //   useRouter: () => ({ replace: jest.fn() }),
 //   useSearchParams: () => ({ get: jest.fn() }),
@@ -61,6 +61,7 @@ describe("SignupForm", () => {
       expect(signupButton).not.toBeInTheDocument();
     });
   });
+
   describe("when empty submit", () => {
     const handleSignupMock = jest.fn();
     beforeEach(() => {
@@ -75,20 +76,67 @@ describe("SignupForm", () => {
     });
 
     it("shows error messages", async () => {
+      const user = userEvent.setup();
       //Arrange
       render(<SignupForm />);
 
       // Act
-      const submitForm = screen.getByRole("form");
-      fireEvent.click(submitForm);
+      const submitButton = screen.getByRole("button", {
+        name: /create new account/i,
+      });
+      await user.click(submitButton);
 
       expect(
-        screen.findByLabelText(/full name must be at least 4 charactersi/i),
+        await screen.findByText(/full name must be at least 4 characters/i),
       );
-      expect(screen.findByLabelText(/invalid email/i));
-      expect(screen.findByLabelText(/password must be at least 8 characters/i));
+      expect(await screen.findByText(/invalid email/i));
+      expect(
+        await screen.findByText(/password must be at least 8 characters/i),
+      );
 
       expect(handleSignupMock).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("when submit correct values", () => {
+    const handleSignupMock = jest.fn();
+    beforeEach(() => {
+      (useAuthActions as jest.Mock).mockReturnValue({
+        isLoading: false,
+        isLoadingGmail: false,
+        handleSignupWithGoogle: jest.fn(),
+        handleSignup: handleSignupMock,
+        setAuthError: jest.fn(),
+        authError: null,
+      });
+    });
+
+    it("shows spinner", async () => {
+      // 1. Setup the user interaction session
+      const user = userEvent.setup();
+
+      //Arrange
+      render(<SignupForm />);
+
+      //Act -  Mandatory: await the action
+      const nameInput = screen.getByLabelText(/full name/i);
+      const emailInput = screen.getByLabelText(/email/i);
+      const passwordInput = screen.getByLabelText(/^password$/i);
+      const confirmPasswordInput = screen.getByLabelText(/^confirm password$/i);
+
+      await user.type(nameInput, "Mohamed Adam");
+      await user.type(emailInput, "test@gmail.com");
+      await user.type(passwordInput, "12345678");
+      await user.type(confirmPasswordInput, "12345678");
+
+      const buttonSubmit = screen.getByRole("button", {
+        name: /create new account/i,
+      });
+
+      await user.click(buttonSubmit);
+
+      //Assert
+      await waitFor(() => expect(handleSignupMock).toHaveBeenCalled());
     });
   });
 });
